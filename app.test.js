56d58cf8476ab0f69f2b98ca67bc09e4363e7662
Akash Agrawal <agrawalakash82@gmail.com>
2024-10-14 20:23:55 +0530

    closing 2

--- app.test.js
+++ app.test.js
@@ -1,16 +1,38 @@
+// const request = require('supertest');
+// const app = require('./app'); // Assuming your Express app is in app.js
+// describe('GET /', () => {
+// it('should return Hello, World!', async () => {
+// const res = await request(app).get('/');
+// expect(res.text).toBe('Hello world');
+// expect(res.statusCode).toBe(200);
+// });
+// });
+
+// afterAll(() => {
+//     server.close();  // Close the server after tests
+//   });
+  
+
+// // new comment 1
+
+
+
 const request = require('supertest');
 const app = require('./app'); // Assuming your Express app is in app.js
-describe('GET /', () => {
-it('should return Hello, World!', async () => {
-const res = await request(app).get('/');
-expect(res.text).toBe('Hello world');
-expect(res.statusCode).toBe(200);
-});
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000);  // Start the server before tests
 });
 
 afterAll(() => {
-    server.close();  // Close the server after tests
-  });
-  
+  server.close();  // Close the server after tests
+});
 
-// new comment 1
+describe('GET /', () => {
+  it('should return Hello, World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toBe('Hello world');
+    expect(res.statusCode).toBe(200);
+  });
+});

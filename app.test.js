3427c856fdda01ad0c1fb45f8853abfc4d15fb62
Akash Agrawal <agrawalakash82@gmail.com>
2024-10-14 20:16:41 +0530

    Hello world

--- app.test.js
+++ app.test.js
@@ -3,7 +3,7 @@ const app = require('./app'); // Assuming your Express app is in app.js
 describe('GET /', () => {
 it('should return Hello, World!', async () => {
 const res = await request(app).get('/');
-expect(res.text).toBe('Hello, World!');
+expect(res.text).toBe('Hello world');
 expect(res.statusCode).toBe(200);
 });
 });
